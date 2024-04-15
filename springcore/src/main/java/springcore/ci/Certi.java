0f340859759a07985480a165e1373651639f02ba
sejalphatangare <sejal.phatangare@gmail.com>
2024-04-15 21:05:28 +0530

    updated

---
+++ springcore/src/main/java/springcore/ci/Certi.java
@@ -0,0 +1,28 @@
+package springcore.ci;
+
+public class Certi {
+	private String name;
+
+	public Certi(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
